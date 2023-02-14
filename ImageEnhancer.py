c8f3a2cb7928ba69e6ce626d0290bf2abb48e883
Edi <82562025+edisaso@users.noreply.github.com>
2023-02-14 08:20:38 +0200

    Update ImageEnhancer.py

--- ImageEnhancer.py
+++ ImageEnhancer.py
@@ -1,14 +1,20 @@
 import os
 import replicate
 import webbrowser
+import  tkinter as tk
+from tkinter import filedialog
+
 os.environ['REPLICATE_API_TOKEN'] = 'Put your replicate.com API token here'
 model = replicate.models.get("tencentarc/gfpgan")
 version = model.versions.get("9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3")
 
+root = tk.Tk()
+root.withdraw()
+PCfile = filedialog.askopenfilename()
 # https://replicate.com/tencentarc/gfpgan/versions/9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3#input
 inputs = {
     # Input
-     'img': open("3.0.PNG", "rb"),
+     'img': open(PCfile, "rb"),
 
     # GFPGAN version. v1.3: better quality. v1.4: more details and better
     # identity.
